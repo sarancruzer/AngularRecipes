0a142bf9ab86e95e7e2b3c9d3efc7a9d9f14fb02
Jeroenouw <jeroenouw@hotmail.com>
2017-02-10 13:35:45 +0100

    Starting point

---
+++ src/app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-header',
+  templateUrl: 'header.component.html'
+})
+export class HeaderComponent {
+
+  constructor() {}
+
+}
